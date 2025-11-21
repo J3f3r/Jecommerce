edbc66858f7690d9b488f98e710c756a3fec54e6
jeferson <jsmartins959@gmail.com>
2025-11-20 22:16:59 -0300

    Adiciona GET Products paged

--- src/main/java/com/jeferson/jecommerce/services/ProductService.java
+++ src/main/java/com/jeferson/jecommerce/services/ProductService.java
@@ -3,10 +3,12 @@ package com.jeferson.jecommerce.services;
 import com.jeferson.jecommerce.dto.ProductDTO;
 import com.jeferson.jecommerce.entities.Product;
 import com.jeferson.jecommerce.repositories.ProductRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 
@@ -22,5 +24,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
