2f4beb57dcfe2afdbb4de362d95114b352375455
jeferson <jsmartins959@gmail.com>
2025-11-25 16:54:21 -0300

    Adiciona Bean Validation

--- src/main/java/com/jeferson/jecommerce/dto/ProductDTO.java
+++ src/main/java/com/jeferson/jecommerce/dto/ProductDTO.java
@@ -2,15 +2,26 @@ package com.jeferson.jecommerce.dto;
 
 
 import com.jeferson.jecommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço precisa ser positivo")
     private Double price;
     private String imgUrl;
 
-    //public ProductDTO(){}
+    public ProductDTO(){}
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
