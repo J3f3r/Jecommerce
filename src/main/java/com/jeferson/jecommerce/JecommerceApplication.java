4c6fa134463464dc6a13e4339cb8c7a91bd2e5d6
jeferson <jsmartins959@gmail.com>
2026-02-10 17:21:27 -0300

    feat(security): configure Spring Security and password encoder BCrypt

--- src/main/java/com/jeferson/jecommerce/JecommerceApplication.java
+++ src/main/java/com/jeferson/jecommerce/JecommerceApplication.java
@@ -1,13 +1,28 @@
 package com.jeferson.jecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class JecommerceApplication {
+public class JecommerceApplication implements CommandLineRunner {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JecommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// esse metodo gera a senha embaralhada pelo BCrypt para ser usado depois
+		System.out.println("Encode = " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "cole o hash aqui");
+		System.out.println("RESULTADO = " + result);
 	}
 
 }
