705f938c64a3817db08a3ed310fb4d0e41cd9b8f
jeferson <jsmartins959@gmail.com>
2026-02-13 21:26:14 -0300

    feat: Authorization server, Resource server, not testees

--- src/main/java/com/jeferson/jecommerce/JecommerceApplication.java
+++ src/main/java/com/jeferson/jecommerce/JecommerceApplication.java
@@ -1,28 +1,12 @@
 package com.jeferson.jecommerce;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class JecommerceApplication implements CommandLineRunner {
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class JecommerceApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JecommerceApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// esse metodo gera a senha embaralhada pelo BCrypt para ser usado depois
-		System.out.println("Encode = " + passwordEncoder.encode("123456"));
-		
-		boolean result = passwordEncoder.matches("123456", "$2a$10$3oWxei.vd8F8iA32BetcOedPSuxtUg2GPE1InPcvcWGm1mbqmYhVq");
-		System.out.println("RESULTADO = " + result);
-	}
-
 }
